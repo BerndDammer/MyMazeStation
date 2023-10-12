5974982aa938bf81b7cb55b9c8f3c35aff05cd90
Bernd Dammer <Bernd.Dammer@web.de>
2023-10-12 19:26:03 +0200

    tyra debug

--- dev_station_raspi/tyra_debug.sh
+++ dev_station_raspi/tyra_debug.sh
@@ -1,1 +1,1 @@
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:2101 -jar tyra.jar
+DISPLAY=:0 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:2101 -jar tyra.jar
--- git_commit_feierabend.sh
+++ git_commit_feierabend.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "vmpk"
+git commit -m "tyra debug"
 git push
 git push --tags
 read
