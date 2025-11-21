4beb91020f16c7b39f230ad2a9d8d10e4d56364c
Bernd Dammer <Bernd.Dammer@web.de>
2025-11-21 22:31:46 +0100

    webcam motion

---
+++ dev_station_left_raspi/copy_watchmen.cmd
@@ -0,0 +1,2 @@
+scp -i aegiselle_sshkey watchmen.jar watchmen.service install_watchmen.sh aegiselle@aekarina.kwangya:~
+pause -----------------------pc-----------------------
---
+++ dev_station_left_raspi/readback_motion_parameters.cmd
@@ -0,0 +1,2 @@
+scp -i aegiselle_sshkey -r aegiselle@aekarina.kwangya:~/.motion/*.* .
+pause ********* XXXXXXXXXXXX **************
